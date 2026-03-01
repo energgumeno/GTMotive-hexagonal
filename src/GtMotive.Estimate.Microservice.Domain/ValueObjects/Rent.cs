2ef37a2c546349bddfeeadea736c549e32644b43
guzman <energ888@gmail.com>
2026-03-01 12:34:19 +0100

    add value objects

---
+++ src/GtMotive.Estimate.Microservice.Domain/ValueObjects/Rent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using GtMotive.Estimate.Microservice.Domain.Entities;
+
+namespace GtMotive.Estimate.Microservice.Domain.Aggregates
+{
+    public class RentInformation : BaseAggregate
+    {
+        //should be moved to user in a bounded context
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+
+        //should be moved to user in a bounded context
+
+        public Guid VehicleId { get; set; }
+        public RentStatus Status { get; set; }
+        public DateTime TimeRentStart { get; set; }
+        public DateTime TimeRentEnd { get; set; }
+
+        public RentInformation(
+            string fullname,
+            string? email,
+            DateTime? timeRentStart,
+            DateTime? timeRentEnd,
+            Guid? vehicleId)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(fullname);
+            ArgumentException.ThrowIfNullOrWhiteSpace(email);
+            if (!timeRentStart.HasValue) throw new ArgumentNullException(nameof(timeRentStart));
+            if (!timeRentEnd.HasValue) throw new ArgumentNullException(nameof(timeRentEnd));
+            if (!vehicleId.HasValue) throw new ArgumentNullException(nameof(vehicleId));
+
+            Fullname = fullname;
+            Email = email;
+            TimeRentStart = timeRentStart ?? DateTime.Now;
+            TimeRentEnd = timeRentEnd ?? DateTime.Now;
+            VehicleId = vehicleId ?? Guid.NewGuid();
+            Status = RentStatus.New;
+        }
+    }
+}
---
+++ src/GtMotive.Estimate.Microservice.Domain/ValueObjects/Vehicle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using GtMotive.Estimate.Microservice.Domain.Entities;
+
+namespace GtMotive.Estimate.Microservice.Domain.Vehicles.ValueObjects
+{
+    /// <summary>
+    /// A vehicle.
+    /// </summary>
+    public class Vehicle : BaseAggregate
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Vehicle"/> class.
+        /// </summary>
+        /// <param name="registrationDate"> registration date.</param>
+        /// <param name="frameId">Frame Id.</param>
+        /// <param name="licensePlate">License plate.</param>
+        private Vehicle(DateTime? registrationDate, string? frameId, string? licensePlate)
+        {
+            registrationDate = registrationDate?.Date;
+            
+            ArgumentException.ThrowIfNullOrWhiteSpace(frameId);
+            ArgumentException.ThrowIfNullOrWhiteSpace(licensePlate);
+            
+            RegistrationDate = registrationDate ?? DateTime.MinValue;
+
+            if (RegistrationDate.AddYears(5) < DateTime.Today)
+            {
+                throw new ArgumentException("Registration date should not be older than 5 years");
+            }
+
+            FrameId = frameId;
+            LicensePlate = licensePlate;
+            Id = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// Gets Registration date.
+        /// </summary>
+        public DateTime RegistrationDate { get; }
+
+        /// <summary>
+        /// Gets Frame id.
+        /// </summary>
+        public string FrameId { get; }
+
+        /// <summary>
+        /// Gets License Plate.
+        /// </summary>
+        public string LicensePlate { get; }
+
+        /// <summary>
+        /// Factory, creates vehicle.
+        /// </summary>
+        /// <param name="registrationDate"> the registration date.</param>
+        /// <param name="frameId"> the frame id.</param>
+        /// <param name="licensePlate"> the license plate.</param>
+        /// <returns> a vehicle.</returns>
+        public static Vehicle Create(DateTime? registrationDate, string? frameId, string? licensePlate)
+        {
+            return new Vehicle(registrationDate, frameId, licensePlate);
+        }
+    }
+}
