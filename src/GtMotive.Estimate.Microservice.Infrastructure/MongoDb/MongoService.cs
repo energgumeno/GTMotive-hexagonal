e527283113b8279e5b2a8a92e2384f1a1f019677
guzman <energ888@gmail.com>
2026-03-02 04:22:36 +0100

    Update MongoDB serializers and class mappings; downgrade Swashbuckle.AspNetCore package version

--- src/GtMotive.Estimate.Microservice.Infrastructure/MongoDb/MongoService.cs
+++ src/GtMotive.Estimate.Microservice.Infrastructure/MongoDb/MongoService.cs
@@ -1,7 +1,10 @@
-﻿using GtMotive.Estimate.Microservice.Domain.ValueObjects;
+﻿using GtMotive.Estimate.Microservice.Domain.Common;
+using GtMotive.Estimate.Microservice.Domain.ValueObjects;
 using GtMotive.Estimate.Microservice.Infrastructure.MongoDb.Settings;
 using Microsoft.Extensions.Options;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 
 namespace GtMotive.Estimate.Microservice.Infrastructure.MongoDb;
@@ -19,12 +22,27 @@ public class MongoService
 
     private static void RegisterBsonClasses()
     {
+        // Se configura la representación de Guid globalmente para evitar errores de serialización.
+        // Se recomienda usar el formato Standard de MongoDB.
+        BsonSerializer.RegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
+
+        if (!BsonClassMap.IsClassMapRegistered(typeof(BaseAggregate)))
+        {
+            BsonClassMap.RegisterClassMap<BaseAggregate>(cm =>
+            {
+                cm.AutoMap();
+                cm.MapIdProperty(c => c.Id);
+            });
+        }
+
         if (!BsonClassMap.IsClassMapRegistered(typeof(Vehicle)))
         {
             BsonClassMap.RegisterClassMap<Vehicle>(cm =>
             {
                 cm.AutoMap();
-                cm.MapIdProperty(c => c.Id);
+                cm.MapProperty(v => v.RegistrationDate);
+                cm.MapProperty(v => v.FrameId);
+                cm.MapProperty(v => v.LicensePlate);
             });
         }
 
@@ -33,7 +51,6 @@ public class MongoService
             BsonClassMap.RegisterClassMap<RentInformation>(cm =>
             {
                 cm.AutoMap();
-                cm.MapIdProperty(c => c.Id);
             });
         }
     }
