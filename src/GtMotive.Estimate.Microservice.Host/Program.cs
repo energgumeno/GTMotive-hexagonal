496ef9239edde70e6f72778443625202265386b7
guzman <energ888@gmail.com>
2026-03-02 04:09:36 +0100

    swagger fix

--- src/GtMotive.Estimate.Microservice.Host/Program.cs
+++ src/GtMotive.Estimate.Microservice.Host/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers();
 builder.Services.AddPresenters(builder.Configuration);
@@ -16,7 +17,12 @@ builder.Services.AddMediatR(typeof(AddVehicleHandler).Assembly);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) app.MapOpenApi();
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.MapControllers();
